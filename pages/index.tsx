a49cddf41748824a34fcead17ee091d406796705
jintao <hellojintao@protonmail.com>
2022-07-13 10:34:29 -0400

    chore: fix build

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,10 @@ const Landing = observer((): JSX.Element => {
   const { protocol } = useContext(StoreContext);
   const networkData = protocol.initialized ? protocol.networks : {};
 
-  const allNetworks = Object.values(networkData).filter((n) => n.vaults.filter((v) => v.state !== VaultState.Discontinued).length > 0);
+  const allNetworks = Object.values(networkData).filter(
+    (n) =>
+      n.vaults.filter((v) => v.state !== VaultState.Discontinued).length > 0,
+  );
   const allVaults = allNetworks.flatMap((v) => v.vaults);
   const totalVaults = allVaults.length;
   const totalValue = allVaults.reduce((total, v) => (total += v.value), 0);
@@ -68,7 +71,7 @@ const Landing = observer((): JSX.Element => {
           />
         </div>
       </div>
-      {Object.keys(networksByTVL).length === 0 && 
+      {Object.keys(networksByTVL).length === 0 && (
         <div className="grid w-full mb-6">
           <div className="flex items-center mb-2">
             <div className="text-xl mr-6">Ethereum</div>
@@ -81,50 +84,54 @@ const Landing = observer((): JSX.Element => {
             <span>Harvest Time</span>
           </div>
           <div className="flex flex-col">
-            {Array.from(new Array(5)).map((_) => {
+            {Array.from(new Array(5)).map((_, i) => {
               return (
-                <div className="bg-deep p-4 shadow-md border-slate border-t hover:bg-slate cursor-pointer grid grid-cols-3 lg:grid-cols-4 w-full">
-                  <span className='rounded-lg h-6 w-32 animate-pulse bg-slate' />
-                  <span className='rounded-lg h-6 w-20 animate-pulse bg-slate' />
-                  <span className='rounded-lg h-6 w-20 animate-pulse bg-slate' />
-                  <span className='rounded-lg h-6 w-32 animate-pulse bg-slate' />
+                <div
+                  key={`vault_skeleton_${i}`}
+                  className="bg-deep p-4 shadow-md border-slate border-t hover:bg-slate cursor-pointer grid grid-cols-3 lg:grid-cols-4 w-full"
+                >
+                  <span className="rounded-lg h-6 w-32 animate-pulse bg-slate" />
+                  <span className="rounded-lg h-6 w-20 animate-pulse bg-slate" />
+                  <span className="rounded-lg h-6 w-20 animate-pulse bg-slate" />
+                  <span className="rounded-lg h-6 w-32 animate-pulse bg-slate" />
                 </div>
-              )
+              );
             })}
           </div>
         </div>
-      }
-      {Object.keys(networksByTVL).length > 0 && networksByTVL
-        .filter((n) => n.tvl > 0 && n.vaults.length > 0)
-        .map((n) => (
-          <div className="grid w-full mb-6" key={n.network}>
-            <div className="flex items-center mb-2">
-              <div className="text-xl mr-6">{n.name}</div>
-              <div className="text-sm font-semibold text-sea">Registry</div>
+      )}
+      {Object.keys(networksByTVL).length > 0 &&
+        networksByTVL
+          .filter((n) => n.tvl > 0 && n.vaults.length > 0)
+          .map((n) => (
+            <div className="grid w-full mb-6" key={n.network}>
+              <div className="flex items-center mb-2">
+                <div className="text-xl mr-6">{n.name}</div>
+                <div className="text-sm font-semibold text-sea">Registry</div>
+              </div>
+              <div className="bg-slate grid grid-cols-3 lg:grid-cols-4 p-4 shadow-lg rounded-t-lg uppercase text-sm text-shallow">
+                <span>Vault</span>
+                <span>TVL</span>
+                <span className="hidden lg:block">APR</span>
+                <span>Harvest Time</span>
+              </div>
+              <div className="flex flex-col">
+                {n.vaults
+                  .slice()
+                  .sort((a, b) => b.value - a.value)
+                  .map((v) => {
+                    const href = `/vault/${n.network}/${v.vaultToken}`;
+                    return (
+                      <Link href={href} key={href} passHref>
+                        <a>
+                          <VaultItem vault={v} />
+                        </a>
+                      </Link>
+                    );
+                  })}
+              </div>
             </div>
-            <div className="bg-slate grid grid-cols-3 lg:grid-cols-4 p-4 shadow-lg rounded-t-lg uppercase text-sm text-shallow">
-              <span>Vault</span>
-              <span>TVL</span>
-              <span className="hidden lg:block">APR</span>
-              <span>Harvest Time</span>
-            </div>
-            <div className="flex flex-col">
-              {n.vaults
-                .slice()
-                .sort((a, b) => b.value - a.value)
-                .map((v) => {
-                  const href = `/vault/${n.network}/${v.vaultToken}`;
-                  return (
-                    <Link href={href} key={href} passHref>
-                      <a>
-                        <VaultItem vault={v} />
-                      </a>
-                    </Link>
-                  );
-                })}
-            </div>
-          </div>
-        ))}
+          ))}
     </div>
   );
 });
