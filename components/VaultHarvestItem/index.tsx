5ba7c7f15cc5a96bb48482619e518d70c0313b44
Entreprenerd <alex@entreprenerd.xyz>
2022-09-23 01:16:37 +0200

    chore: lint

--- components/VaultHarvestItem/index.tsx
+++ components/VaultHarvestItem/index.tsx
@@ -21,54 +21,56 @@ function VaultHarvestItem({ harvestInformation }: Props): JSX.Element {
     <div className="my-1 w-full lg:w-10/12">
       <Link href={href} passHref>
         <a>
-        <div className="flex bg-card rounded-lg p-1 lg:p-3 cursor-pointer mx-2">
-          <div className="flex flex-col items-center justify-center w-16 md:w-24 m-2">
-            <div
-              className={`w-2 h-2 rounded-full ${
+          <div className="flex bg-card rounded-lg p-1 lg:p-3 cursor-pointer mx-2">
+            <div className="flex flex-col items-center justify-center w-16 md:w-24 m-2">
+              <div
+                className={`w-2 h-2 rounded-full ${
+                  realizedHarvestPercent > 100
+                    ? 'bg-electric'
+                    : realizedHarvestPercent > 97.5
+                    ? 'bg-green-400'
+                    : realizedHarvestPercent > 95
+                    ? 'bg-orange-400'
+                    : 'bg-red-400'
+                }`}
+              />
+              <div className="text-xs text-gray-400">{`${
                 realizedHarvestPercent > 100
-                  ? 'bg-electric'
+                  ? 'Excellent'
                   : realizedHarvestPercent > 97.5
-                  ? 'bg-green-400'
+                  ? 'Good'
                   : realizedHarvestPercent > 95
-                  ? 'bg-orange-400'
-                  : 'bg-red-400'
-              }`}
-            />
-            <div className="text-xs text-gray-400">{`${
-              realizedHarvestPercent > 100
-                ? 'Excellent'
-                : realizedHarvestPercent > 97.5
-                ? 'Good'
-                : realizedHarvestPercent > 95
-                ? 'Watch'
-                : 'Investigate'
-            }`}</div>
-          </div>
-          <div className="flex flex-grow w-full">
-            <div className="grid grid-cols-2 md:grid-cols-4 w-full">
-              <div className="flex flex-col">
-                <div className="text-lg">{name}</div>
-                <div className="text-xs text-gray-400">{networkName}</div>
-              </div>
-              <div className="flex flex-col">
-                <div className="text-lg">
-                  {realizedHarvestPercent.toFixed(2)}%
+                  ? 'Watch'
+                  : 'Investigate'
+              }`}</div>
+            </div>
+            <div className="flex flex-grow w-full">
+              <div className="grid grid-cols-2 md:grid-cols-4 w-full">
+                <div className="flex flex-col">
+                  <div className="text-lg">{name}</div>
+                  <div className="text-xs text-gray-400">{networkName}</div>
                 </div>
-                <div className="text-xs text-gray-400">
-                  Realized Harvest Value
+                <div className="flex flex-col">
+                  <div className="text-lg">
+                    {realizedHarvestPercent.toFixed(2)}%
+                  </div>
+                  <div className="text-xs text-gray-400">
+                    Realized Harvest Value
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col">
-                <div className="text-lg">{formatter.format(yieldValue)}</div>
-                <div className="text-xs text-gray-400">Yield Value</div>
-              </div>
-              <div className="flex flex-col">
-                <div className="text-lg">{formatter.format(harvestValue)}</div>
-                <div className="text-xs text-gray-400">Harvest Value</div>
+                <div className="flex flex-col">
+                  <div className="text-lg">{formatter.format(yieldValue)}</div>
+                  <div className="text-xs text-gray-400">Yield Value</div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="text-lg">
+                    {formatter.format(harvestValue)}
+                  </div>
+                  <div className="text-xs text-gray-400">Harvest Value</div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         </a>
       </Link>
     </div>
